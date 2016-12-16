2bec8d9facf17dc2f94399c9e933e4eb5790bc5e
Erick Katzenstein <ekatzenstein@modelab.is>
2016-12-16 12:59:07 -0500

    react port - first attempt

--- js/models/Githubapi.js
+++ js/models/Githubapi.js
@@ -1,140 +1,89 @@
-function commitChanges(ob) {
-    require(["githubapi"], function (Github) {
-        var token;
-        $j.getJSON("./configuration/config.json", function (result) {
-            token = result["GitHub_Token"];
-            // This is a personal access token, not using oAuth.
-            // Currently this is under ramramps.  We have  to create
-            // a generic user id and add to Github collobrator.
-            // Add the token to a config file and put it in S3. Do not share the token
-            var github = new Github({
-                token: token
-            });
-            //step 1: Create the branch
-            //TO DO : Generate unique name for the branch
-            var repo = github.getRepo("DynamoDS", "DynamoDictionary");
-            var pull;
+function githubSubmitter(files, mainExampleFile, branchName, message, terminate) {
+    axios.get('./configuration/config.json').then((resolve, reject) => {
+        const token = resolve.data.GitHub_Token;
+        runSubmit(token, files, mainExampleFile, branchName, message, terminate)
+    })
+}
+function dynContents(f) {
+    console.log('dyn run',f)
+    var file_path = 'public/' + f.og.slice(2).replace("Examples", "EXAMPLES");
+    var fileOb = {
+        content: f.data,
+        isBase64: false
+    };
+    return [file_path, fileOb];
+}
 
-            function commitFile(data) {
-                //currently do a commit for each file (completed in the map function below)
-                var options = {
-                    author: {
-                        name: 'User'
-                        , email: 'user@example.com'
-                    }
-                    , committer: {
-                        name: 'User'
-                        , email: 'user@example.com'
-                    }
-                    , encode: data.enc // Whether to base64 encode the file. (default: true) 
-                };
-                repo.writeFile(gitInfo.branchName, data.if, data.i, gitInfo.Message, options, function (err) {
-                    if (err) {
-                        console.log(err)
-                        onGHError()
-                    }
-                    else {
-                        if (prd == false) {
-                            pr();
-                            prd = true;
-                        }
-                    }
-                });
-            }
-            repo.createBranch("master", gitInfo.branchName, function (err) {
-                //create a branch
-                var filelist = [];
-                filelist[0] = {
-                        i: ob
-                        , if: 'data/Dynamo_Nodes_Documentation.json'
-                        , enc: true
-                    }
-                    //eOb represents all of the nodes that have been changed.
-                for (key in eOb) {
-                    console.log(eOb)
-                    for (keykey in eOb[key]) {
-                        var mainOb = JSON.parse(ob);
-                        var nodeOb = mainOb[key];
-                        var subub = eOb[key][keykey]
-                        if(subub.imageFile){
-                            
-                        }
-                        
-                        console.log(subub)
-                        if (subub.image && subub.imageFile) {
-                            var imageFile = 'data/EXAMPLES/' + nodeOb.folderPath + "/img/" + subub.imageFile.split('.')[0] + ".jpg";
-                            filelist.push({
-                                i: window.btoa(window.atob((subub.image.replace(/^(.+,)/, ''))))
-                                , if: imageFile
-                                , enc: false
-                            });
-                        }
-                        if (subub.dyn && subub.dynFile) {
-                            var dynFile = 'data/EXAMPLES/' + nodeOb.folderPath + "/dyn/" + subub.dynFile.split('.')[0] + ".dyn";
-                            filelist.push({
-                                i: subub.dyn
-                                , if: dynFile
-                                , enc: true
-                            });
-                        }
-                    }
-                }
-                if (filelist.length > 0) {
-                    filelist.map(commitFile);
-                }
-            });
-
-            function viewPR(url) {
-                //view pull request after creation
-                var SM = new SimpleModal({
-                    "btn_ok": "Commit Edits"
-                });
-                SM.options.draggable = false;
-                SM.show({
-                    "title": '<a href="https://github.com/DynamoDS/DynamoDictionary" target=_blank><img src="images/src/icon.png" width="30" alt="dynamoIcon" align="middle" target="_blank" style="vertical-align:middle;"></a>&nbsp<span>Pull Request Created!</span>'
-                    , "contents": "Pull Request created! Thank you for your patience while we review before updating the site. <a href='" + url + "' target=_blank style='color:black;'>Follow your PR on Github here.</a>"
-                , });
-                //            d3.selectAll(".close").on('click',function(){location.reload()})
-                //            d3.selectAll("#simple-modal-overlay").on('click',function(){location.reload()})
-            }
-
-            function onGHError() {
-                //error logging
-                var SM = new SimpleModal({
-                    "btn_ok": "Commit Edits"
-                });
-                SM.options.draggable = false;
-                SM.show({
-                    "title": '<a href="https://github.com/DynamoDS/DynamoDictionary" target=_blank><img src="images/src/icon.png" width="30" alt="dynamoIcon" align="middle" target="_blank" style="vertical-align:middle;"></a>&nbsp<span>Pull Request Error</span>'
-                    , "contents": "There was an issue submitting the Pull Request to the Github repo. Visit the <a href='https://github.com/DynamoDS/DynamoDictionary' target='_blank' style='color:red;'>Dynamo Dictionary repo</a> to submit an issue or a manual pull request."
-                , });
-            }
-
-            function pr() {
-                //create pull request
-                repo.getContents(gitInfo.branchName, "README.md", true, function (err, contents) {
-                    var pull = {
-                        title: gitInfo.branchName
-                        , body: "This pull request has been automatically generated by DynamoDictionaryUser."
-                        , base: "master"
-                        , head: gitInfo.branchName
-                    };
-                    repo.createPullRequest(pull, function (err, pullRequest) {
-                        if (err) {
-                            onGHError()
-                        }
-                        else {
-                            viewPR(pullRequest.html_url);
-                            submittedpr = true;
-                            d3.select('#prLogo').attr("src", "images/icons/gh.png")
-                            d3.select('#submitPR').select("span").html("View PR on Github! &nbsp;&nbsp;&nbsp;")
-                            d3.select('#submitPR').on("click", function () {
-                                window.location = (pullRequest.html_url)
-                            })
-                        }
-                    });
-                })
-            }
-        });
+function imageContents(f) {
+    console.log('img run',f)
+    var file_path = 'public/' + f.og.slice(2).replace("Examples", "EXAMPLES");
+    var file_contents = window.atob((f.data.replace(/^(.+,)/, '')));
+    var fileOb = {
+        content: file_contents,
+        isBase64: true
+    };
+    return [file_path, fileOb];
+}
+function runSubmit(token, files, mainExampleFile, branchName, message, terminate) {
+    var branchExists = false;
+    files.push(mainExampleFile)
+    var gh = new Octokit({
+        token: token
     });
+    var repo = gh.getRepo('ekatzenstein', 'DynamoDictionary_React');
+    repo.getBranches()
+        .then(function(branches) {
+            if (branches.filter(function(b) {
+                    return b[2] === branchName;
+                }).length > 0) {
+                console.log('branchExists')
+                branchExists = true;
+                commitFiles(repo.getBranch(branchName));
+            } else {
+                console.log('branch does not exist')
+                var branch = repo.getBranch();
+                branch.createBranch(branchName)
+                    .then(function(b) {
+                        commitFiles(repo.getBranch(branchName))
+                    })
+            }
+        })
+    function commitFiles(branch) {
+        if (files.length > 0) {
+            var contents = {};
+            files.forEach(function(f) {
+                switch (f.type) {
+                    case 'image':
+                        var ob = imageContents(f);
+                        contents[ob[0]] = ob[1];
+                        break;
+                    case 'xml':
+                        var ob = dynContents(f);
+                        contents[ob[0]] = ob[1];
+                        break;
+                    default:
+                        contents['public/data/Dynamo_Nodes_Documentation.json'] = JSON.stringify(f, null, 4);
+                        return undefined;
+                }
+            })
+            branch.writeMany(contents, message)
+                .then(function() {
+                    if (!branchExists) {
+                        return repo.createPullRequest({
+                            "title": branchName,
+                            "body": "files updated by user",
+                            "head": branchName,
+                            "base": "master"
+                        })
+                    } else {
+                        return function() {
+                            console.log('no pr created')
+                        }
+                    }
+                })
+                .then(function(result) {
+                    terminate(result.html_url);
+                })
+        }
+    }
 }
